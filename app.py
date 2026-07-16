80b0bc7f6e04f286ee66f99fde8e9d3f39767925
rathiii12 <ihtarrathi5@gmail.com>
2026-07-16 19:07:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+
+    if request.method == "POST":
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operation = request.form["operation"]
+
+        if operation == "+":
+            result = num1 + num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "*":
+            result = num1 * num2
+        elif operation == "/":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Cannot divide by zero"
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
